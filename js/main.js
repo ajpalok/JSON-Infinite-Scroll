f34e8f093218c53620c863d7801de1116b67891d
Abrar Jahin <66951426+ajpalok@users.noreply.github.com>
2021-10-14 17:56:05 +0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -12,7 +12,7 @@ $(document).ready(function(){
 	var scrollTolerance = 600; // Define range of pixels between scroll position and bottom page as a tolerance to trigger the function
 	var elementsToAppendPerScroll = 4; // Define the elements to index when user reach bottom of the site
 
-	$.getJSON( "js/ajax/signatories.json", function(data) {
+	$.getJSON( "https://ajpalok.github.io/JSON-Infinite-Scroll/js/ajax/signatories.json", function(data) {
 		console.log("json data: " + data.signatories.length);
 		signatories_in_json = data.signatories.length;
 
